a0748c7493fc3fad1650b16b639979fa4b869675
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-07-04 02:19:20 +0000

    Build manifest on deployment

--- packages/gengi-is/bin/deploy-live.sh
+++ packages/gengi-is/bin/deploy-live.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 gulp build --prod
+gulp manifest --prod
 rsync -azv -O --chmod g+w dist/* vps01.kolibri.is:/www/gengi-is/live
 gulp notify --title 'Deployment' --msg 'Successfully deployed to live'
