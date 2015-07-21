6c837ffd53e677a742ee68df107c2a446509c01a
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-07-21 16:39:01 +0000

    Add fallback to manifest task

--- packages/gengi-is/tasks/manifest.js
+++ packages/gengi-is/tasks/manifest.js
@@ -14,6 +14,7 @@ module.exports = function(gulp) {
         // timestamp: true,
         preferOnline: true,
         network: gulp.cfg.manifest.network,
+        fallback: gulp.cfg.manifest.fallback,
         filename: gulp.cfg.manifest.filename,
         exclude: gulp.cfg.manifest.exclude,
       }))
