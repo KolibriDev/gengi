1eb42ea715c0472d96c38c2b394e52f10edf10d0
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-07-21 16:41:01 +0000

    Run require script before manifest in deployment

--- packages/gengi-is/bin/deploy-live.sh
+++ packages/gengi-is/bin/deploy-live.sh
@@ -2,6 +2,7 @@
 
 gulp clean --prod
 gulp build --prod
+./bin/require.sh
 gulp manifest --prod
 rsync -azv -O --chmod g+w dist/* vps01.kolibri.is:/www/gengi-is/live
 gulp notify --title 'Deployment' --msg 'Successfully deployed to live'
