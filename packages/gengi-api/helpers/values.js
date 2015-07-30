5bb4b45e920cddb0539cb21ae245c0647ff9ce6f
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-07-30 23:45:40 +0000

    Remove duplicates in currency names

--- packages/gengi-api/helpers/values.js
+++ packages/gengi-api/helpers/values.js
@@ -3,6 +3,12 @@ exports.name = function(name){
   if (!name) { return name; }
   name = name.split(',');
   name = name.length > 1 ? name[1].trim() + ' ' + name[0].trim() : name[0];
+
+  if (name.indexOf(' ') > -1) {
+    var split = name.split(' ');
+    name = name.replace((split[split.length-1] + ' ' + split[split.length-1]), split[1]);
+  }
+
   return name;
 };
 
