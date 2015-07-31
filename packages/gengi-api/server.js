22c1246afaf745fc867890100723c6eb6c787c6b
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-07-31 02:36:06 +0000

    Make sure server.js is jshint compliant

--- packages/gengi-api/server.js
+++ packages/gengi-api/server.js
@@ -1,15 +1,14 @@
 var express = require('express');
 
 var http = require('http'),
-    path = require('path'),
     app = express();
 
 app.set('port', process.env.PORT || 8002);
 
 // Allow all origins
 app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 
