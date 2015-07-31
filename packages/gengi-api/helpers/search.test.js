e8b38605fe4382c09ea31177ebf02d2069f679fd
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-07-31 02:48:07 +0000

    Create tests for search helper function

---
+++ packages/gengi-api/helpers/search.test.js
@@ -0,0 +1,36 @@
+var search = require('./search');
+var assert = require('assert');
+var _ = require('underscore');
+
+describe('Search', function(){
+
+  describe('filter()', function(){
+    var arr = [
+      { code: 'USD', name: 'bandarískur dalur' },
+      { code: 'DKK', name: 'dönsk króna' },
+      { code: 'CAD', name: 'kanadískur dalur' },
+      { code: 'SEK', name: 'sænsk króna' },
+      { code: 'NOK', name: 'norsk króna' },
+    ];
+    var retval = _.filter(arr, function(value){
+      return search.filter(value, 'króna');
+    });
+    var expectedval = [
+      { code: 'DKK', name: 'dönsk króna' },
+      { code: 'SEK', name: 'sænsk króna' },
+      { code: 'NOK', name: 'norsk króna' },
+    ];
+
+    it('should return an array', function(done){
+      assert(Array.isArray(retval), 'retval should be an array');
+      done();
+    });
+
+    it('should return filtered list by term', function(done){
+      assert(_.isEqual(retval, expectedval), 'should equal expectedval');
+      done();
+    });
+
+  });
+
+});
