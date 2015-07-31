d9b04f3ab8195fd796c5f1c52e319f5e7273f8db
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-07-31 02:46:35 +0000

    Auto include test files from endpoints & helpers

--- packages/gengi-api/test.js
+++ packages/gengi-api/test.js
@@ -1,3 +1,23 @@
 process.env.NODE_ENV = 'testing';
 
-// Add tests here
+var fs = require('fs');
+
+describe('Endpoints',function(){
+  require('fs').readdirSync('./endpoints/').forEach(function(file) {
+    if (file.indexOf('.test') > -1) {
+      if(fs.existsSync('./endpoints/' + file)){
+        require('./endpoints/' + file);
+      }
+    }
+  });
+});
+
+describe('Helpers',function(){
+  require('fs').readdirSync('./helpers/').forEach(function(file) {
+    if (file.indexOf('.test') > -1) {
+      if(fs.existsSync('./helpers/' + file)){
+        require('./helpers/' + file);
+      }
+    }
+  });
+});
