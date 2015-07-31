6e1dd1cc3f535e5c2d3680b80992931e42d4753c
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-07-31 01:42:18 +0000

    Ensure values provided to search.filter are right

--- packages/gengi-api/helpers/search.js
+++ packages/gengi-api/helpers/search.js
@@ -4,10 +4,12 @@
   Compare search term with available fields and return true for any partial match.
 **/
 exports.filter = function(value, term){
-  if (value.code.toUpperCase().indexOf(term) > -1) { return true; }
-  if (value.name.toUpperCase().indexOf(term) > -1) { return true; }
-  if (value.country.toUpperCase().indexOf(term) > -1) { return true; }
-  if (value.countryCode.toUpperCase().indexOf(term) > -1) { return true; }
-  if (value.countryEnglish.toUpperCase().indexOf(term) > -1) { return true; }
+  term = term ? term.toUpperCase() : false;
+  if (!term) { return false; }
+  if (value.hasOwnProperty('code') && value.code.toUpperCase().indexOf(term) > -1) { return true; }
+  if (value.hasOwnProperty('name') && value.name.toUpperCase().indexOf(term) > -1) { return true; }
+  if (value.hasOwnProperty('country') && value.country.toUpperCase().indexOf(term) > -1) { return true; }
+  if (value.hasOwnProperty('countryCode') && value.countryCode.toUpperCase().indexOf(term) > -1) { return true; }
+  if (value.hasOwnProperty('countryEnglish') && value.countryEnglish.toUpperCase().indexOf(term) > -1) { return true; }
   return false;
 };
