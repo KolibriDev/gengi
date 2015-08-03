24f2bdb177bd16e9d30012874d2aef8a578372b5
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-08-03 17:50:05 +0000

    Set up data structure + fetch from new api

--- packages/gengi-is/src/js/gengi.js
+++ packages/gengi-is/src/js/gengi.js
@@ -1,4 +1,4 @@
-define(['vue'], function(Vue) {
+define(['vue', 'promise'], function(Vue, promise) {
   'use strict';
   var _gengi = {
     vm: false,
@@ -6,9 +6,95 @@ define(['vue'], function(Vue) {
       _gengi.vm = new Vue({
         el: 'gengi',
         data: {
-          title: 'Gengi.is',
+          state: '', // loading, expired, ready
+          expires:  _gengi.initData('expires'),
+          app: _gengi.initData('app'),
+          currencies: _gengi.initData('currencies'),
+          currencyDate:  _gengi.initData('currencyDate'),
+          currencyList:  _gengi.initData('currencyList'),
         },
       });
+      _gengi.vm.$watch('app', function(){
+        _gengi.storeDataLocally('app');
+      });
+      _gengi.vm.$watch('expires', function(){
+        _gengi.storeDataLocally('expires');
+      });
+      _gengi.vm.$watch('currencies', function(){
+        _gengi.storeDataLocally('currencies');
+      });
+      _gengi.vm.$watch('currencyDate', function(){
+        _gengi.storeDataLocally('currencyDate');
+      });
+      _gengi.vm.$watch('currencyList', function(){
+        _gengi.storeDataLocally('currencyList');
+      });
+      if (_gengi.vm.currencies.length < 1) {
+        _gengi.getCurrencies();
+      }
+    },
+
+    defaults: {
+      app: {
+        title: 'Gengi.is...',
+      },
+      expires: 0,
+      currencies: [],
+      currencyDate: 0,
+      currencyList: [
+        'eur',
+        'usd',
+        'gbp',
+        'dkk',
+        'sek',
+        'nok',
+        'pln',
+      ],
+    },
+
+    getCurrencies: function(){
+      promise.get(
+        'http://api-v2.gengi.is/currency/' + _gengi.vm.currencyList.join(',')
+      ).then(function(error, response, xhr){
+        if (error) {
+          console.error('Error ' + xhr.status);
+          return;
+        }
+        var res = JSON.parse(response);
+        _gengi.vm.$set('expires', res.expires);
+        _gengi.vm.$set('currencies', res.currencies);
+        _gengi.vm.$set('currencyDate', res.currencyDate);
+      });
+      return {};
+    },
+
+    initData: function(dataName){
+      var data = _gengi.getLocalData(dataName) || _gengi.defaults[dataName] || {};
+      _gengi.storeDataLocally(dataName,data);
+      return data;
+    },
+    getLocalData: function(dataName){
+      var data = window.localStorage.getItem(dataName);
+      if (data) {
+        try {
+          data = JSON.parse(data);
+          if (dataName === 'currencies' && _gengi.vm.expires < new Date().getTime()) { // 1 day
+            throw 'too old!';
+          }
+          return data;
+        } catch(exc) {
+          _gengi.clearLocalData(dataName);
+          return dataName === 'currencies' ? [] : false;
+        }
+      }
+      return dataName === 'currencies' ? [] : false;
+    },
+    clearLocalData: function(dataName) {
+      window.localStorage.removeItem(dataName);
+    },
+    storeDataLocally: function(dataName, data) {
+      data = data || _gengi.vm.hasOwnProperty(dataName) && _gengi.vm[dataName];
+      window.localStorage.setItem(dataName, JSON.stringify(data));
     },
   };
 
