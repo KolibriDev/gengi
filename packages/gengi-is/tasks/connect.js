943af6c01586dcfb3541dea77255dd535a3de5a1
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-08-04 14:52:08 +0000

    Implement connect history api fallback

--- packages/gengi-is/tasks/connect.js
+++ packages/gengi-is/tasks/connect.js
@@ -1,11 +1,16 @@
 'use strict';
 
 module.exports = function(gulp) {
+  var historyFallback = require('connect-history-api-fallback');
   gulp.task('connect', function() {
     return gulp.plugin.connect.server({
       root: [gulp.cfg.env.dir],
       port: gulp.cfg.connect.port,
-      livereload: gulp.cfg.connect.livereload
+      livereload: gulp.cfg.connect.livereload,
+
+      middleware: function() {
+        return [ historyFallback() ];
+      }
     });
   });
 };
