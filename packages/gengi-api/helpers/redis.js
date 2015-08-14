349c19821c9794cf1ee70156cc464efce9505350
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-08-14 02:29:47 +0000

    Guard against no redis, resolves #6

--- packages/gengi-api/helpers/redis.js
+++ packages/gengi-api/helpers/redis.js
@@ -2,23 +2,59 @@ var redisClient = require('redis').createClient();
 var time = require('./time');
 
 var redis = {};
+redis.ready = false;
+redis.error = '';
 redis.cacheKey = 'mastercard-gengi';
 
+
+redisClient.on('error', function (err) {
+  console.log('Error ' + err);
+  redis.error = err;
+  redis.ready = false;
+});
+redisClient.on('ready', function () {
+  redis.error = '';
+  redis.ready = true;
+});
+
+redis.isReady = function(callback) {
+  if(redis.ready) {
+    callback();
+  } else {
+    callback({error: redis.error});
+  }
+};
+
 redis.set = function(data) {
-  redisClient.setex(redis.cacheKey, time.secsToMidnight(), JSON.stringify(data));
+  redis.isReady(function(err){
+    if (!err) {
+      redisClient.setex(redis.cacheKey, time.secsToMidnight(), JSON.stringify(data));
+    }
+  });
 };
 
 redis.get = function(callback) {
-  redisClient.get(redis.cacheKey, function(err, results) {
+  redis.isReady(function(err){
     if (err) {
       callback(err);
+    } else {
+      redisClient.get(redis.cacheKey, function(err, results) {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null,JSON.parse(results));
+        }
+      });
     }
-    callback(null,JSON.parse(results));
   });
 };
 
 redis.clear = function() {
-  return redisClient.del(redis.cacheKey);
+  redis.isReady(function(err){
+    if (!err) {
+      redisClient.del(redis.cacheKey);
+    }
+  });
 };
 
 module.exports = redis;
