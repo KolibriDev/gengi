1babb8fdef8b48bb997721fd747179197d298ee1
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-08-14 01:49:13 +0000

    Create basic test for currencies.ensureCodes()

--- packages/gengi-api/test/helper-currencies.test.js
+++ packages/gengi-api/test/helper-currencies.test.js
@@ -31,4 +31,31 @@ describe('Helpers > currencies', function(){
 
   });
 
+  describe('ensureCodes(codes, currencies)', function(){
+
+    it('should exist', function(done){
+      assert(currencies.hasOwnProperty('ensureCodes'), 'expected to have property');
+      done();
+    });
+
+    it('should be a function', function(done){
+      assert(typeof currencies.ensureCodes === 'function', 'expected currencies.ensureCodes to be a function');
+      done();
+    });
+
+    var value = currencies.ensureCodes('usd,eur');
+    console.log(value);
+
+    it('should return an array', function(done){
+      assert(Array.isArray(value), 'expected to be array');
+      done();
+    });
+
+    it('should be have a length of 2, and include USD and EUR', function(done){
+      assert(value.length === 2 && value.indexOf('USD') > -1 && value.indexOf('EUR') > -1, 'expected \'["USD","EUR"]\', got \''+value+'\'');
+      done();
+    });
+
+  });
+
 });
