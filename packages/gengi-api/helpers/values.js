3be9e35b5fa9bacc4dd969a2d30911963862017d
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-08-18 15:17:05 +0000

    Fix processing currency name
    Was returning `hollenskt antilluantillugyllini` instead of `hollenskt antillugyllini`

--- packages/gengi-api/helpers/values.js
+++ packages/gengi-api/helpers/values.js
@@ -12,7 +12,7 @@ exports.name = function(name){
   if (name.indexOf(' ') > -1) {
     // Remove duplicates in name, e.g. 'sterlingspund pund'
     var split = name.split(' ');
-    name = name.replace((split[split.length-1] + ' ' + split[split.length-1]), split[1]);
+    name = name.replace((split[split.length-1] + ' ' + split[split.length-1]), split[split.length-1]);
   }
 
   return name;
