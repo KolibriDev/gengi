5fdcea609cf2c9bf9cb997e71d7fe93f5d3ce4ec
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-08-19 01:08:46 +0000

    Refactor sanitize as es6 module

--- packages/gengi-is/src/js/utils/sanitize.js
+++ packages/gengi-is/src/js/utils/sanitize.js
@@ -1,20 +1,19 @@
 'use strict';
 define([], () => {
-  var _san = {
-    code: (code) => {
+  var Sanitize = class {
+    code(code) {
       return code.indexOf('NAN') > -1 && code.length > 3 ? code.replace('NAN','') : code;
-    },
-    number: (value) => {
+    }
+    number(value) {
       if (value.toString().indexOf(',')) {
         value = value.toString().replace(',', '.');
       }
       value = parseFloat(value);
-      value = isNaN(value) || value <= 0 ? 1 : value;
+      value = isNaN(value) || value < 0 ? 1 : value;
 
       return value;
-    },
-    format: () => {},
+    }
   };
 
-  return _san;
+  return new Sanitize();
 });
