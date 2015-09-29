e729d43947c9610b82cbf4b0ef5422f9ed628a55
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-09-29 17:08:44 +0000

    Add global module

---
+++ packages/gengi-is/src/js/modules/global.js
@@ -0,0 +1,61 @@
+import $ from 'zepto';
+import _ from 'underscore';
+
+let Global = class {
+  constructor() {
+    this.options = {
+      debug: false,
+    };
+
+    this.attributes = {};
+
+    this.createEls();
+  }
+
+  createEls() {
+    this.els = {
+      overlay: $('<overlay />'),
+    };
+    $.each(this.els, (index, el) => {
+      $('body').append(el);
+    });
+  }
+
+  getEl(name) {
+    if (!name || !this.els.hasOwnProperty(name)) { return undefined; }
+    return this.els[name];
+  }
+
+  setAttr(key, value) {
+    if (!key) { return false; }
+
+    key = this.cleanValue(key);
+    value = this.cleanValue(value);
+
+    $('html').attr('data-' + key, value);
+    this.attributes[key] = value;
+    return true;
+  }
+
+  getAttr(key) {
+    if (!key) { return undefined; }
+    key = this.cleanValue(key);
+    return this.attributes[key];
+  }
+  clearAttr(key) {
+    if (!key) { return; }
+    $('html').removeAttr('data-' + key);
+    this.attributes = _.omit(this.attributes, key);
+  }
+  clearAllAttr() {
+    _.each(this.attributes, (value, key) => {
+      this.clear(key);
+    });
+  }
+
+  cleanValue(value) {
+    return value && value.toString().toLowerCase();
+  }
+};
+
+export default new Global();
