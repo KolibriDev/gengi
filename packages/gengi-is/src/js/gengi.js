365f4611406efb8ffc9c87b15cf4fe6cbd7c26cd
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-09-30 15:58:43 +0000

    WIP: Rewrite gengi main file

--- packages/gengi-is/src/js/gengi.js
+++ packages/gengi-is/src/js/gengi.js
@@ -1,10 +1,83 @@
 'use strict';
-import jquery from 'vendor/jquery'; // jshint ignore:line
+import global from 'global';
+/* jshint ignore:start */
+// These vendor modules register themselves globally
+import jquery from 'vendor/jquery';
+import underscore from 'vendor/underscore';
+/* jshint ignore:end */
+import storage from 'modules/storage';
+import sanitize from 'modules/sanitize';
+import currencies from 'modules/currencies';
+import calculate from 'modules/calculate';
 
 class Gengi {
   constructor() {
-    this.foo = 'bar';
     console.info('Final rewrite (yeah right)');
+    this.version = '0.0.8';
+    this.syncStorage();
+
+    global.setAttr('foo', 'bar');
+
+    // this.state = 'loading';
+  }
+
+  calculate() {
+    console.group('Calculating');
+    let rate = this.currencies.list['USD'].rate;
+    let amount = this.amount.cur;
+
+    let retval = calculate(rate, amount);
+    console.log('retval: ', retval);
+    console.groupEnd('and done!');
+  }
+
+  search() {}
+
+  setIsk(newValue) {
+    this.amount.isk = sanitize.number(newValue) || this.amount.isk;
+    this.store('amount');
+  }
+  setCur(newValue) {
+    this.amount.cur = sanitize.number(newValue) || this.amount.cur;
+    this.store('amount');
+  }
+
+  store(target) {
+    if (!target) { return undefined; }
+    storage.set(target, this[target]);
+  }
+
+  syncStorage() {
+    if (storage.get('version') !== this.version) {
+      storage.clearAll();
+    }
+    this.amount = {
+      isk: 0,
+      cur: 1,
+    };
+
+    this.selectedCurrencies = this.selectedCurrencies || [
+      'EUR',
+      'USD',
+      'GBP',
+    ];
+
+    this.store('amount');
+    this.store('version');
+    this.store('selectedCurrencies');
+
+    currencies.get((res) => {
+      this.currencies = res;
+    }, (res) => {
+      if (!res) {
+        // TODO: inform user their screwed
+        console.warn('inform user their screwed');
+      } else {
+        // TODO: inform user they have expired rates
+        console.warn('inform user they have expired rates');
+        this.currencies = res;
+      }
+    });
   }
 }
 
---
+++ packages/gengi-is/src/js/modules/currencies.js
@@ -0,0 +1,37 @@
+import jquery from 'vendor/jquery'; // jshint ignore:line
+
+import storage from 'modules/storage';
+
+export default {
+  source: 'http://api-v2.gengi.is/currencies',
+
+  get: function(success, fail) {
+    success = success && typeof success === 'function' ? success : () => {};
+    fail = fail && typeof fail === 'function' ? fail : () => {};
+
+    let storedCurrencies = storage.get('currencies');
+    if (storedCurrencies && storedCurrencies.expires >= new Date().getTime()) {
+      success(storedCurrencies);
+      return;
+    }
+
+    $.ajax({
+      method: 'GET',
+      url: this.source,
+    }).done((data) => {
+      if (!data || !data.hasOwnProperty('list')) {
+        fail(storedCurrencies);
+        return;
+      }
+      try {
+        let res = JSON.parse(data.response);
+        storage.set('currencies', res);
+        success(res);
+      } catch(exc) {
+        fail(storedCurrencies);
+      }
+    }).fail(() => {
+      fail(storedCurrencies);
+    });
+  },
+};
