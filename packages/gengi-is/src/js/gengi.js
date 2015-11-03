f5b0d0779ccf293735eea92db936279f0174da4c
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-11-03 15:47:17 +0000

    WIP: Disable sorting on 'all currencies'

--- packages/gengi-is/src/js/gengi.js
+++ packages/gengi-is/src/js/gengi.js
@@ -43,43 +43,79 @@ class Gengi {
       global.clearAttr('load-step');
       global.setAttr('state', event.type);
 
-      if (event.type === 'leaving') {
-        console.log(event.type, 'view', global.getAttr('view'));
-      }
-      if (event.type === 'loading') {
-      }
-      if (event.type === 'loaded' && global.getAttr('view') !== 'home' && global.getAttr('view') !== 'allcurrencies'  && global.getAttr('edit-mode') === 'true') {
-        this.disableEdit();
-      }
       if (event.type === 'loaded') {
-        this.initSorting();
         this.swiftclick.replaceNodeNamesToTrack(['currency','curr-selected','curr-next','div','num']);
+
+        this.initSorting();
+
+        if (global.getAttr('edit-mode') === 'true' && global.getAttr('view') !== 'home' && global.getAttr('view') !== 'allcurrencies') {
+          this.disableEdit();
+        }
       }
 
-      if (global.getAttr('view') === 'home' && global.getAttr('edit-mode') === 'true') {
-        this.enableEdit();
+      if (global.getAttr('view') === 'home') {
+        if (global.getAttr('edit-mode') === 'true') {
+          this.enableEdit();
+          this.enableSorting();
+        } else {
+          this.disableEdit();
+          this.softdisableSorting();
+        }
+      } else {
+        this.disableEdit();
+        this.disableSorting();
       }
     });
 
     $('[edit]').off('click.edit').on('click.edit', () => {
       this.enableEdit();
+      if (global.getAttr('view') === 'home') {
+        this.enableSorting();
+      } else {
+        this.disableSorting();
+      }
     });
     $('[done]').off('click.done').on('click.done', () => {
       this.disableEdit();
+      if (global.getAttr('view') === 'home') {
+        this.softdisableSorting();
+      } else {
+        this.disableSorting();
+      }
     });
   }
 
   enableEdit() {
+    // console.trace('enableEdit');
     global.setAttr('edit-mode', true);
-    // this.sortingOptions({
-    //   'delay': 0,
-    // });
   }
   disableEdit() {
+    // console.trace('disableEdit');
     global.setAttr('edit-mode', false);
-    // this.sortingOptions({
-    //   'delay': 250,
-    // });
+  }
+
+  enableSorting() {
+    // console.trace('enableSorting');
+    this.sortingOptions({disabled: true});
+    this.sortingOptions({
+      delay: 0,
+      disabled: false,
+    });
+  }
+
+  disableSorting() {
+    // console.trace('disableSorting');
+    this.sortingOptions({disabled: true});
+  }
+
+  softdisableSorting() {
+    // console.trace('softdisableSorting');
+    global.setAttr('edit-mode', false);
+    this.sortingOptions({disabled: true});
+    this.sortingOptions({
+      delay: 250,
+      disabled: false,
+    });
   }
 
   sortingOptions(options) {
@@ -94,8 +130,8 @@ class Gengi {
     this.sortableOptions = {
       group: 'currency-list',
       sort: true,
-      delay: 10,
-      disabled: false,
+      delay: 250,
+      disabled: true,
       animation: 150,
       draggable: 'currency',
       ghostClass: 'sortable-ghost',
@@ -110,11 +146,13 @@ class Gengi {
       },
 
       onUpdate: () => {
-        let currlist = [];
-        $('currency-list > div.list > currency').each((i, el) => {
-          currlist.push($(el).attr('code'));
-        });
-        currencies.reorderSelected(currlist);
+        if (global.getAttr('view') === 'home') {
+          let currlist = [];
+          $('currency-list > div.list > currency').each((i, el) => {
+            currlist.push($(el).attr('code'));
+          });
+          currencies.reorderSelected(currlist);
+        }
       },
     };
     $.extend(this.sortableOptions, options);
