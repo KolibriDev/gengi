1fe3ad52302754303cd4737d4de4c01917abfc92
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-10-05 14:58:29 +0000

    Combine index files into ./lib

---
+++ packages/gengi-app/lib/index.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class gengiapp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit lib/index.js
+        </Text>
+        <Text style={styles.instructions}>
+          iOS: Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu {'\n\n'}
+
+          Android: Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('gengiapp', () => gengiapp);
