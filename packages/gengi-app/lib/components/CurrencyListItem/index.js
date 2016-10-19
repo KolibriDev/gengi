b8462ccd312181653b2745a1c624088aea8dda8c
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-10-19 16:56:13 +0000

    Add symbols to CurrencyListItem resolves #7

--- packages/gengi-app/lib/components/CurrencyListItem/index.js
+++ packages/gengi-app/lib/components/CurrencyListItem/index.js
@@ -9,6 +9,7 @@ import styles from '../../styles'
 export default (props) => {
   return (
     <View style={styles.currencyListItem}>
+      <Text style={styles.currencyListSymbol}>{props.currency.symbol}</Text>
       <Text style={styles.currencyListTitle}>{props.currency.code}</Text>
       <Text style={styles.currencyListName}>{props.currency.name}</Text>
       <Text style={styles.currencyListRate}>{Math.round(props.currency.rate * 100) / 100}</Text>
