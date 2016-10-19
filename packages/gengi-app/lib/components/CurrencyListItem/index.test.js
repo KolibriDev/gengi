93977724164e8232fe293775d0adc3cdb789aedb
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-10-19 17:52:55 +0000

    Implement basic jest test env resolves #3

---
+++ packages/gengi-app/lib/components/CurrencyListItem/index.test.js
@@ -0,0 +1,6 @@
+
+describe('CurrencyListItem component', () => {
+  test.skip('Missing test', () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ packages/gengi-app/lib/components/Header/index.test.js
@@ -0,0 +1,6 @@
+
+describe('Header component', () => {
+  test.skip('Missing test', () => {
+    expect(true).toBe(true)
+  })
+})
